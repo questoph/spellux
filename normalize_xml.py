110a372cc2ea1dc706992ea43a3246cb8b5e1543
questoph <christoph@purschke.info>
2020-03-20 12:30:57 +0100

    Update normalize_xml.py

--- normalize_xml.py
+++ normalize_xml.py
@@ -1,27 +1,33 @@
 # Script to auto-normalize xml files from the STRIPS project
 
 import spellux
+import os
 import glob
 import argparse
 import xml.etree.ElementTree as ET
 from progressbar import ProgressBar
 pbar = ProgressBar()
 
+thedir = os.path.dirname(__file__)
 
 # Define argument parser to pass args with script
 parser = argparse.ArgumentParser('Auto-normalize xml files')
-parser.add_argument("--dir", type=str, help='Specify the path to the directory')
+parser.add_argument("--indir", type=str, help='Specify the path to the directory')
 parser.add_argument("--tag", type=str, default='w', help='Specify the xml tag to process (default= "w")')
+parser.add_argument("--outdir", type=str, help='Specify the path to the saving directory')
 args = parser.parse_args()
 
 # Set list of files for normalization
-files = glob.glob(args.dir + "/*.xml")
+xmldir = args.indir
+ending = "*.xml"
+path = os.path.join(str(xmldir), ending)
+files = glob.glob(path)
 print("\nNumber of files to normalize: {}" .format(str(len(files))))
 
 # Start correction routine
 for file in files:
     print("\nCorrecting: " + file)
-    
+
     # Parse xml file and structure
     tree = ET.parse(file)
     root = tree.getroot()
@@ -31,11 +37,10 @@ for file in files:
         text = entry.text
         corrected = spellux.normalize_text(text, mode="model", add_matches=True, sim_ratio=75, stats=False, print_unknown=False, nrule=False, indexing=False, lemmatize=False, tolist=False, progress=False)
         entry.set("corr", corrected)
-    
+
     name = file[4:-4] + "_normalized"
     suff = file[-4:]
-    tree.write(args.dir + "normalized/" + name + suff)
+    tree.write(args.outdir + name + suff)
     print("File done!\n")
 
-spellux.global_stats()
 print("All files corrected!")
